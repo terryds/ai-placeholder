7a92dbe40161017a890a63caccf2e9a6b8215c74
user <terrydjony@gmail.com>
2023-05-28 19:09:33 +0700

    debug console log env, will delete letter

--- openaiApi.js
+++ openaiApi.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 import { load } from "dotenv";
 const env = await load();
 
-
+console.log(env)
 const configuration = new Configuration({
     apiKey: env["OPENAI_API_KEY"],
   });
