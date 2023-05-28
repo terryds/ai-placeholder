9da71a1d27cc2d891e9cd1e3fcdf366f00c902df
user <terrydjony@gmail.com>
2023-05-28 19:16:17 +0700

    debug console log env, will delete later

--- openaiApi.js
+++ openaiApi.js
@@ -2,10 +2,12 @@ import { Configuration, OpenAIApi } from "openai";
 import { load } from "dotenv";
 const env = await load();
 
-console.log(env)
 const configuration = new Configuration({
-    apiKey: env["OPENAI_API_KEY"],
+    apiKey: env["OPENAI_API_KEY"] || Deno.env.get("OPENAI_API_KEY"),
   });
 const openaiApi = new OpenAIApi(configuration);
+
+console.log(env["OPENAI_API_KEY"])
+console.log(`Deno.env: ${Deno.env.get("OPENAI_API_KEY")}`)
 
 export default openaiApi;
